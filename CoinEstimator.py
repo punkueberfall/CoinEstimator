57e1ad3a3240b1ee9a01602dccf113bc3c4ff81c
Stefan Zuegg <stefan.zuegg@gmail.com>
2016-02-15 12:46:06 +0100

    finished project

--- CoinEstimator.py
+++ CoinEstimator.py
@@ -27,7 +27,7 @@ Subgoals
 
     Allow the user to select whether they want to submit the weight in either grams or pounds.
 ================================================================================================================"""
-
+import math
 print "This is a coin estimator"
 
 def wahl():
@@ -37,6 +37,7 @@ wgt_coins=[2.5,5.,2.268,5.67]
 worth_coins=[0.01,0.05,0.1,0.25]
 wrapper_list=[50,40,50,40]
 choice=wahl()
+
 
 wgt_pennies=raw_input("weight of pennies: ")
 wgt_nickels=raw_input("weight of nickels: ")
@@ -48,28 +49,51 @@ if choice=="g":
     amount_nickels=int(float(wgt_nickels)//wgt_coins[1])
     amount_dimes=int(float(wgt_dimes)//wgt_coins[2])
     amount_quarters=int(float(wgt_quarters)//wgt_coins[3])
+
+    wrapper_p=math.ceil(float(amount_pennies)/wrapper_list[0])
+    wrapper_n=math.ceil(float(amount_nickels)/wrapper_list[1])
+    wrapper_d=math.ceil(float(amount_dimes)/wrapper_list[2])
+    wrapper_q=math.ceil(float(amount_quarters)/wrapper_list[3])
+
     money_pen=amount_pennies*worth_coins[0]
     money_nic=amount_nickels*worth_coins[1]
     money_dime=amount_dimes*worth_coins[2]
     money_qua=amount_quarters*worth_coins[3]
     money=money_pen+money_nic+money_dime+money_qua
 
-    print "\npennies: "+str(amount_pennies)+"\nnickels: "+str(amount_nickels)+"\ndimes: "+str(amount_dimes)+"\nquarters: "+str(amount_quarters)+"\ntotal amount of money: "+str(money)
+    print "\npennies: "+str(amount_pennies)+" equals: "+str(int(wrapper_p))+" wrappers"+\
+          "\nnickels: "+str(amount_nickels)+" equals: "+str(int(wrapper_n))+" wrappers"+\
+          "\ndimes: "+str(amount_dimes)+" equals: "+str(int(wrapper_d))+" wrappers"+\
+          "\nquarters: "+str(amount_quarters)+" equals: "+str(int(wrapper_q))+" wrappers"+\
+          "\ntotal amount of money: "+str(money)+" usd"
+
 elif choice=="lbs":
     wgt_pennies*=453.592
     wgt_nickels*=453.592
     wgt_dimes*=453.592
     wgt_quarters*=453.592
+
     amount_pennies=int(float(wgt_pennies)//wgt_coins[0])
     amount_nickels=int(float(wgt_nickels)//wgt_coins[1])
     amount_dimes=int(float(wgt_dimes)//wgt_coins[2])
     amount_quarters=int(float(wgt_quarters)//wgt_coins[3])
+
+    wrapper_p=math.ceil(float(amount_pennies)/wrapper_list[0])
+    wrapper_n=math.ceil(float(amount_nickels)/wrapper_list[1])
+    wrapper_d=math.ceil(float(amount_dimes)/wrapper_list[2])
+    wrapper_q=math.ceil(float(amount_quarters)/wrapper_list[3])
+
     money_pen=amount_pennies*worth_coins[0]
     money_nic=amount_nickels*worth_coins[1]
     money_dime=amount_dimes*worth_coins[2]
     money_qua=amount_quarters*worth_coins[3]
     money=money_pen+money_nic+money_dime+money_qua
-    print "\npennies: "+str(amount_pennies)+"\nnickels: "+str(amount_nickels)+"\ndimes: "+str(amount_dimes)+"\nquarters: "+str(amount_quarters)+"\ntotal amount of money: "+str(money)
+
+    print "\npennies: "+str(amount_pennies)+" equals: "+str(int(wrapper_p))+" wrappers"+\
+          "\nnickels: "+str(amount_nickels)+" equals: "+str(int(wrapper_n))+" wrappers"+\
+          "\ndimes: "+str(amount_dimes)+" equals: "+str(int(wrapper_d))+" wrappers"+\
+          "\nquarters: "+str(amount_quarters)+" equals: "+str(int(wrapper_q))+" wrappers"+\
+          "\ntotal amount of money: "+str(money)+" usd"
 else:
     print "Wrong Choice for weight unit"
 
